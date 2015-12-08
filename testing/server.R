6ba5648e75cdddd81d4e8658f93e78031bdf90fd
russelljohnston <rwi.johnston@gmail.com>
2015-12-08 21:12:45 +0000

    update to zoopla API access

--- testing/server.R
+++ testing/server.R
@@ -12,131 +12,133 @@ shinyServer(function(input, output) {
   
   observeEvent(input$do, {
     
+    bedMin     <- reactive({bMi=input$minBed})
+    bedMax     <- reactive({bMa=input$maxBed})
+    furnished  <- reactive({fur=input$furnished})
+    priceMin   <- reactive({pMi=input$minPr/4.})
+    priceMax   <- reactive({pMa=input$maxPr/4.})
+    searchRad  <- reactive({sRad=input$searchR})
+    loc        <- reactive({locc=input$location})
+    propType   <- reactive({pTy=input$type})
+  
+    
+    key <- "fzzfvsuf37rgy8k2vfruxcwd"
+    
+    listing_status="rent"
+    url="http://api.zoopla.co.uk/api/v1/property_listings.xml"
+    
+    sample <- GET(url,  query = list(
+      area = loc(),
+      listing_status="rent",
+      page_size=100,
+      page_number=1,
+      property_type=propType(),
+      furnished=furnished(),
+      minimum_beds=bedMin(),
+      maximum_beds=bedMax(),
+      minimum_price=priceMin(),
+      maximum_price=priceMax(),
+      radius=searchRad(),
+      summarised="true",
+      order_by="age",
+      include_rented=1,
+      api_key = key))
+    
+    result   <- content(sample)
+    rootNode <- xmlRoot(result)
+    price    <- data.frame(as.numeric(xpathSApply(rootNode,"//rental_prices/per_month",xmlValue)))
+    colnames(price) = 'price'
+    nbed     <- data.frame(as.integer(xpathSApply(rootNode,"//num_bedrooms",xmlValue)))
+    colnames(nbed) = 'number_of_bedrooms'
+    listUrl  <- xpathSApply(rootNode,"//details_url",xmlValue)
     
     output$histrent <- renderPlot({
       
-      bedMin <- reactive({bMi=input$minBed})
-      bedMax <- reactive({bMa=input$maxBed})
-      
-      furnished       <- reactive({fur=input$furnished}) 
-      includeRented   = "true"
-      pageSize        = "100"
-      priceFreq       = "per_month"
-      priceMin        <- reactive({pMi=input$minPr}) 
-      priceMax        <- reactive({pMa=input$maxPr}) 
-      searchRad       <- reactive({sRad=input$searchR})
-      
-      # loc="Oxford%20Station%2C%20Oxfordshire"
-      loc="Reading%20Station"
-       
-      url <- "http://www.zoopla.co.uk/to-rent/flats/station/rail/reading/"
-      response <- GET(url,query=list(
-        beds_max=bedMax(),
-        beds_min=bedMin(),
-        furnished_state=furnished(),
-        include_rented=includeRented,
-        page_size=pageSize,
-        price_frequency=priceFreq,
-        price_min=priceMin(),
-        price_max=priceMax(),
-        q=loc,
-        radius=searchRad()
-      ))
-      
-      html <- htmlTreeParse(response, useInternalNodes = T)
-      rootNode <- xmlRoot(html)
-      hrefs = xpathSApply(html, '//*[@class="listing-results-attr"]/a',xmlGetAttr,'href')
-      stations = unique(xpathSApply(html,"//*[@class='nearby_stations_schools_name']",xmlValue))
-      nbed =  data.frame(as.integer(xpathSApply(html, '//*[@class="num-icon num-beds"]',xmlValue)))
-      colnames(nbed) = 'number_of_bedrooms'
-      
-      price = xpathSApply(html,"//div[@class='listing-results-right']",xmlValue)
-      # price =  xpathSApply(html, '//*[@class="listing-results-price text-price"]',xmlValue)
-      price <- gsub("^.*\\£ *(.*) * pcm.*$", "\\1", price)
-      price <- as.numeric(gsub(",","", price))
+      validate(
+        need(priceMax()-priceMin() >=0., "'Max Price' must be greater than or equal to 'Min Price' "),
+        need(bedMax()-bedMin() >=0., "'Max Beds' must be greater than or equal to 'Min Beds' "),
+        need(nrow(price)>5, "Less than 5 properties found in this price range. Try increasing Max Price" )
+      )
       
       
-      if(length(price) < 5) {
-        output$pErr <- renderText({ 
-          paste("less than 5 properties found in this price range. Try increasing Max Price")
-        })
-        output$meanTxt <- renderText({paste("")})
-        output$medTxt  <- renderText({paste("")})
-      } else {
-        
-        bed.df <- data.frame(price,nbed)
-        bed.df$number_of_bedrooms <- gsub('([0-9])',"\\1 bedroom", bed.df$number_of_bedrooms)
-        
-        countTot<-aggregate(bed.df[, 1], list(bed.df$number_of_bedrooms), function(x) length(unique(x)))
-        colnames(countTot)=c('Number of Bedrooms','Total Number of Properties')
-        meanPrice<-aggregate(bed.df[, 1], list(bed.df$number_of_bedrooms), mean)
-        colnames(meanPrice)=c('Number of Bedrooms','Average Price (£)')
-        medPrice<-aggregate(bed.df[, 1], list(bed.df$number_of_bedrooms), median)
-        colnames(medPrice)=c('Number of Bedrooms','Median Price (£)')
-        allPrice <- merge(meanPrice,medPrice,by='Number of Bedrooms')
-        allPrice <- merge(allPrice,countTot,by='Number of Bedrooms')
-        
-        g<- ggplot(bed.df, aes(price, fill = number_of_bedrooms)) + geom_density(alpha = 0.5) 
-        g<- ggplotGrob(g)  
-        g<- gtable_add_rows(g, unit(1.5,"in"), 0) 
-        g<- gtable_add_grob(g, tableGrob(allPrice, rows = rownames(allPrice), cols = colnames(allPrice)),
-                             t = 1, l=4, b=1, r=4)
-        g<- gtable_add_rows(g, unit(1,"in"), 0) 
-        histtitle = paste("Properties near",loc,"(sourced from Zoopla)",sep=" ")
-        title_style <- g$grobs[[8]]$gp
-        g <- gtable_add_grob(g, textGrob(histtitle, x=0, hjust=0, gp=title_style),
-                              t=1, l=4, b=1, r=4, name="hist-title")
-        
-        # grid.newpage()
-        # grid.draw(g)
-        
-        output$pErr    <- renderText({paste("")})
-        
-        #Now Query Google maps for transit times from this area
-        
-        #get nearby stations to properties 
-        stations = unique(xpathSApply(html,"//*[@class='nearby_stations_schools_name']",xmlValue))
-        gStations = paste(stations, collapse = '|') 
-        
-        
-        mode="transit"
-        transit_mode="rail"
-        to="Farringdon"
-        from=gStations
-        region="uk"
-        key="AIzaSyDi5gaiVz3Eq3xMg6ndUC5X3bhQLxY_F4w" 
-        url    <- "https://maps.googleapis.com/maps/api/distancematrix/xml"
-        response <- GET(url,query=list(
-          origins=from,
-          destinations=to,
-          region=region,
-          mode=mode,
-          transit_mode=transit_mode,
-          key=key
-        ))
-        doc      <- content(response,type="text/xml")
-        status   <- sapply(doc["//row/element/status"],xmlValue)
-        if(any(status!="OK")) warning("Error Status on some routes")
-        durationText <- sapply(doc["//row/element/duration/text"],xmlValue)
-        durationNum <- as.numeric(sapply(doc["//row/element/duration/value"],xmlValue))/60
-        
-        print(durationText)
-        
-        df_train<- data.frame(stations,durationText,as.integer(durationNum))
-        colnames(df_train) = c("Stations", "Total Transit time", "(mins)") 
-        
-        tt <- tableGrob(df_train)
-        # grid.newpage()
-        grid.arrange(g,tt,nrow=2,ncol=1)
-        
-
-      }
-    }, height = 700, width = 600)
+      bed.df <- data.frame(price,nbed)
+      bed.df$number_of_bedrooms <- gsub('([0-9])',"\\1 bedroom", bed.df$number_of_bedrooms)
+      
+      
+      countTot<-aggregate(bed.df[, 1], list(bed.df$number_of_bedrooms), function(x) length(unique(x)))
+      colnames(countTot)=c('Number of Bedrooms','Total Number of Properties')
+      meanPrice<-aggregate(bed.df[, 1], list(bed.df$number_of_bedrooms), mean)
+      colnames(meanPrice)=c('Number of Bedrooms','Average Price (£)')
+      medPrice<-aggregate(bed.df[, 1], list(bed.df$number_of_bedrooms), median)
+      colnames(medPrice)=c('Number of Bedrooms','Median Price (£)')
+      allPrice <- merge(meanPrice,medPrice,by='Number of Bedrooms')
+      allPrice <- merge(allPrice,countTot,by='Number of Bedrooms')
+      
+      
+      g<- ggplot(bed.df, aes(price, fill = number_of_bedrooms)) + geom_density(alpha = 0.5) 
+      g<- ggplotGrob(g)  
+      g<- gtable_add_rows(g, unit(1.5,"in"), 0) 
+      g<- gtable_add_grob(g, tableGrob(allPrice, rows = rownames(allPrice), cols = colnames(allPrice)),
+                          t = 1, l=4, b=1, r=4)
+      g<- gtable_add_rows(g, unit(1,"in"), 0) 
+      
+      
+      histtitle = paste(propType()," near",loc(),"(sourced from Zoopla)",sep=" ")
+      title_style <- g$grobs[[8]]$gp
+      g <- gtable_add_grob(g, textGrob(histtitle, x=0, hjust=0, gp=title_style),
+                           t=1, l=4, b=1, r=4, name="hist-title")
+      
+      # grid.newpage()
+      grid.draw(g)
+      # link = response[1]
+      
+      # output$zoopres <- renderUI({ a(href=link) })
+      output$pErr    <- renderText({paste("")})
+      
+    }, height = 400, width = 600)
     
-#     output$transtable <- renderPlot({
-#       
-#       
-#     })
+    #     output$transtable <- renderPlot({
+    #       
+    #       #Now Query Google maps for transit times from this area
+    #       
+    #       #get nearby stations to properties 
+    #       stations = unique(xpathSApply(html,"//*[@class='nearby_stations_schools_name']",xmlValue))
+    #       gStations = paste(stations, collapse = '|') 
+    #       
+    #       
+    #       mode="transit"
+    #       transit_mode="rail"
+    #       to="Farringdon"
+    #       from=gStations
+    #       region="uk"
+    #       key="AIzaSyDi5gaiVz3Eq3xMg6ndUC5X3bhQLxY_F4w" 
+    #       url    <- "https://maps.googleapis.com/maps/api/distancematrix/xml"
+    #       response <- GET(url,query=list(
+    #         origins=from,
+    #         destinations=to,
+    #         region=region,
+    #         mode=mode,
+    #         transit_mode=transit_mode,
+    #         key=key
+    #       ))
+    #       doc      <- content(response,type="text/xml")
+    #       status   <- sapply(doc["//row/element/status"],xmlValue)
+    #       if(any(status!="OK")) warning("Error Status on some routes")
+    #       durationText <- sapply(doc["//row/element/duration/text"],xmlValue)
+    #       durationNum <- as.numeric(sapply(doc["//row/element/duration/value"],xmlValue))/60
+    #       
+    #       print(durationText)
+    #       
+    #       df_train<- data.frame(stations,durationText,as.integer(durationNum))
+    #       colnames(df_train) = c("Stations", "Total Transit time", "(mins)") 
+    #       
+    #       tt <- tableGrob(df_train)
+    #       # grid.newpage()
+    #       # grid.arrange(g,tt,nrow=2,ncol=1)
+    #       grid.draw(tt)
+    #       
+    #       
+    #     }, height = 200, width = 600)
     
   })
 })
--- testing/ui.R
+++ testing/ui.R
@@ -7,29 +7,33 @@ shinyUI(fluidPage(
   titlePanel("Can I Live In London?"),
   
   sidebarLayout(
-    sidebarPanel(
+    sidebarPanel(width=3,
     
-
+        textInput("location",
+                  label = h6("Area Interested in"),
+                  value = "Oxford"),
         numericInput("minPr", 
                    label = h6("Min Price pcm (£)"), 
                    min = 0, max = 5000, value = 100, step = 250), 
     
         numericInput("maxPr", 
                  label = h6("Max Price pcm (£)"), 
-                 min = 0, max = 5000, value = 1000, step = 250), 
+                 min = 0, max = 5000, value = 2000, step = 250), 
+        radioButtons("type",label = h6("Property Type"),
+                     choices = list("houses","flats")),
         numericInput("minBed", 
                      label = h6("Min Beds"), 
                      min = 0, max = 5, value = 1, step = 1), 
         numericInput("maxBed", 
                      label = h6("Max Beds"), 
-                     min = 0, max = 5, value = 1, step = 1), 
+                     min = 0, max = 5, value = 2, step = 1), 
         
         radioButtons("furnished",label = h6(""),
                      choices = list("furnished","unfurnished")),
         
         selectInput("searchR", 
                     label = h6("Search Radius (miles)"), 
-                               choices = c('1'='1','3'='3','5'='5','10'='10','15'='15','20'='20','30'='30','40'='40')),
+                               choices = c('1/4'='0.25','1/2'='0.5','1'='1','3'='3','5'='5','10'='10','15'='15','20'='20','30'='30','40'='40')),
         
         actionButton("do", "Click Me")
   ),
@@ -37,8 +41,10 @@ shinyUI(fluidPage(
     
     mainPanel(
       plotOutput("histrent",width = "100%"),
-      textOutput("pErr")
-      # plotOutput("transtable")
+      textOutput("zoopres"),
+      textOutput("pErr"),
+      textOutput("priceErr"),
+      plotOutput("transtable")
               )
    )
 ))
